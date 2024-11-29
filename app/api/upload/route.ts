78cf032903079f5540d9858308e1f8c301c7fdea
PC1 <umdomby@yandex.ru>
2024-11-29 18:15:00 +0300

    edit

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,21 +1,27 @@
-import { NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
-import fs from "node:fs/promises";
+import { put } from '@vercel/blob';
+import { NextResponse } from 'next/server';
 
-export async function POST(req: Request) {
-    try {
-        const formData = await req.formData();
+export async function POST(request: Request): Promise<NextResponse> {
+    const { searchParams } = new URL(request.url);
+    const filename = searchParams.get('filename');
 
-        const file = formData.get("file") as File;
-        const arrayBuffer = await file.arrayBuffer();
-        const buffer = new Uint8Array(arrayBuffer);
-        await fs.writeFile(`./public/${file.name}`, buffer);
+    // ⚠️ The below code is for App Router Route Handlers only
+    // @ts-ignore
+    const blob = await put(filename, request.body, {
+        access: 'public',
+    });
 
-        revalidatePath("/");
+    // Here's the code for Pages API Routes:
+    // const blob = await put(filename, request, {
+    //   access: 'public',
+    // });
 
-        return NextResponse.json({ status: "success" });
-    } catch (e) {
-        console.error(e);
-        return NextResponse.json({ status: "fail", error: e });
-    }
+    return NextResponse.json(blob);
 }
+
+// The next lines are required for Pages API Routes only
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
