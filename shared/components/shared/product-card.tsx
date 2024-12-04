d8c1bc72f9632058e158a3568e063eec27e55c94
PC1 <umdomby@yandex.ru>
2024-12-04 19:23:36 +0300

    edit height

--- shared/components/shared/product-card.tsx
+++ shared/components/shared/product-card.tsx
@@ -4,6 +4,15 @@ import { Title } from './title';
 import { Button } from '../ui';
 import { Plus } from 'lucide-react';
 import { Ingredient } from '@prisma/client';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/shared/components/ui/table"
 
 interface Props {
   id: number;
@@ -25,26 +34,33 @@ export const ProductCard: React.FC<Props> = ({
   return (
     <div className={className}>
       <Link href={`/product/${id}`}>
-        <Title text={name} size="sm" className="mb-1 mt-1 font-bold" />
-        <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
-          <img className="w-[215px] h-[215px]" src={imageUrl} alt={name} />
-        </div>
+        {/*<div className="flex justify-center p-1 bg-secondary rounded-lg h-[95%]">*/}
+          <div className="justify-items-center content-center p-1 bg-secondary rounded-lg h-[100%]">
+              <Title text={name} size="xs" className="font-bold line-clamp-1 ml-2"/>
+              <img className="w-[90%] h-[88%]" src={imageUrl} alt={name}/>
 
-        {/*<p className="text-sm text-gray-400">*/}
-        {/*  {ingredients.map((ingredient) => ingredient.name).join(', ')}*/}
-        {/*</p>*/}
 
-        {/*<div className="flex justify-between items-center mt-4">*/}
-        {/*  <span className="text-[20px]">*/}
-        {/*    от <b>{price} ₽</b>*/}
-        {/*  </span>*/}
+            {/*<div>*/}
 
-        {/*  <Button variant="secondary" className="text-base font-bold">*/}
-        {/*    <Plus size={20} className="mr-1" />*/}
-        {/*    Добавить*/}
-        {/*  </Button>*/}
-        {/*</div>*/}
+            {/*</div>*/}
+
+          </div>
+
+          {/*<p className="text-sm text-gray-400">*/}
+          {/*  {ingredients.map((ingredient) => ingredient.name).join(', ')}*/}
+          {/*</p>*/}
+
+          {/*<div className="flex justify-between items-center mt-4">*/}
+          {/*  <span className="text-[20px]">*/}
+          {/*    от <b>{price} ₽</b>*/}
+          {/*  </span>*/}
+
+          {/*  <Button variant="secondary" className="text-base font-bold">*/}
+          {/*    <Plus size={20} className="mr-1" />*/}
+          {/*    Добавить*/}
+          {/*  </Button>*/}
+          {/*</div>*/}
       </Link>
     </div>
-  );
+);
 };
