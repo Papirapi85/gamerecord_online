d2e0f9baa71e602ca802edb15c492e890d0af308
PC1 <umdomby@yandex.ru>
2024-12-08 00:42:34 +0300

    edit

--- shared/components/shared/products-group-list.tsx
+++ shared/components/shared/products-group-list.tsx
@@ -41,16 +41,16 @@ export const ProductsGroupList: React.FC<Props> = ({
       <Title text={title} size="lg" className="font-extrabold mb-5" />
 
       <div className={cn('grid grid-cols-3 gap-[5px]', listClassName)}>
-        {items.map((product, i) => (
-          <ProductCard
-            key={product.id}
-            id={product.id}
-            name={product.name}
-            imageUrl={product.imageUrl}
-            price={product.items[0].price}
-            ingredients={product.ingredients}
-          />
-        ))}
+        {/*{items.map((product, i) => (*/}
+        {/*  <ProductCard*/}
+        {/*    key={product.id}*/}
+        {/*    id={product.id}*/}
+        {/*    name={product.name}*/}
+        {/*    imageUrl={product.imageUrl}*/}
+        {/*    price={product.items[0].price}*/}
+        {/*    ingredients={product.ingredients}*/}
+        {/*  />*/}
+        {/*))}*/}
       </div>
     </div>
   );
